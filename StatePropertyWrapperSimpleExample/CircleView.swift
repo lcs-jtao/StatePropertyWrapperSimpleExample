ff85c4452eea95f9f3f9e21164b132aac833d653
Joyce Tao <jtao@lcs.on.ca>
2021-11-29 12:00:05 -0500

    Added @State and limited keyboard type for the text field

--- StatePropertyWrapperSimpleExample/CircleView.swift
+++ StatePropertyWrapperSimpleExample/CircleView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 struct CircleView: View {
     
     // MARK: Stored properties
-    let radius: Double = 10.0
+    // @State is a "property wrapper"
+    // "radius" is still just a property of the structure
+    // The property wrapper alerts SwiftUI to the fact that we want change to this property to show in the UI
+    @State var radius: Double = 10.0
     
     // MARK: Computed properties
     var area: Double {
@@ -26,6 +29,7 @@ struct CircleView: View {
             TextField("Radius",
                       text: .constant(""),
                       prompt: Text("e.g.: 24.5"))
+                .keyboardType(.decimalPad)
             
             // Output
             Text("Area:")
